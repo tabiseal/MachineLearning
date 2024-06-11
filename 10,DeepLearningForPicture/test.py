d9f97a62a4b40f38ef754291b555b3030ef2cef5
tabiseal <115387977+tabiseal@users.noreply.github.com>
2024-06-12 00:54:33 +0900

    Add files via upload

--- 10,DeepLearningForPicture/test.py
+++ 10,DeepLearningForPicture/test.py
@@ -21,6 +21,7 @@ plt.show()
 X_train=X_train.reshape(60000,784)/255.0
 X_test=X_test.reshape(10000,784)/255.0
 
+#把Y_train和Y_test变成了One-hot编码
 Y_train=to_categorical(Y_train,10)
 Y_test=to_categorical(Y_test,10)
 
@@ -28,12 +29,12 @@ model=Sequential()
 model.add(Dense(units=256,activation='relu',input_dim=784))
 model.add(Dense(units=256,activation='relu'))
 model.add(Dense(units=256,activation='relu'))
-model.add(Dense(units=10,activation='softmax'))
+model.add(Dense(units=10,activation='softmax'))#使用softmax函数使得10个输出的相加概率和为1
 
 
 model.compile(loss='categorical_crossentropy',optimizer=SGD(learning_rate=0.05),metrics=['accuracy'])
 
-model.fit(X_train,Y_train,epochs=5000,batch_size=2048)
+model.fit(X_train,Y_train,epochs=5000,batch_size=128)
 
 loss,accuracy=model.evaluate(X_test,Y_test)
 
