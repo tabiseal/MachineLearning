0ad5b3714872cbab32c0051bf5fb7dd8a5e5a8f4
tabiseal <115387977+tabiseal@users.noreply.github.com>
2024-06-12 23:43:06 +0900

    Add files via upload

--- 11,CNN/test.py
+++ 11,CNN/test.py
@@ -15,16 +15,25 @@ from keras.layers import Flatten
 
 X_train=X_train.reshape(60000,28,28,1)/255.0
 X_test=X_test.reshape(10000,28,28,1)/255.0
+#60000枚の図/大きさ28*28/チャンネル数:1;/255.0正規化操作
 
 Y_train=to_categorical(Y_train,10)
 Y_test=to_categorical(Y_test,10)
 
 model=Sequential()
 model.add(Conv2D(filters=6, kernel_size=(5,5) ,strides=(1,1),input_shape=(28,28,1), padding='valid',activation='relu'))
+#filters畳み込みフィルタ
+#kernel_size畳み込みのサイズ
+#stridesストライド(畳み込みフィルタ毎回移動する長さ)
+#input_shape入力形状
+#paddingパディングモード
+#activation活性化関数
 model.add(AveragePooling2D(pool_size=(2,2)))
-model.add(Conv2D(filters=6, kernel_size=(5,5) ,strides=(1,1), padding='valid',activation='relu'))
+model.add(Conv2D(filters=16, kernel_size=(5,5) ,strides=(1,1), padding='valid',activation='relu'))
 model.add(AveragePooling2D(pool_size=(2,2)))
 model.add(Flatten())
+
+
 model.add(Dense(units=120,activation='relu'))
 model.add(Dense(units=84,activation='relu'))
 model.add(Dense(units=10,activation='softmax'))
@@ -36,8 +45,8 @@ model.fit(X_train,Y_train,epochs=500,batch_size=2048)
 loss,accuracy=model.evaluate(X_test,Y_test)
 print("loss"+str(loss))
 print("accuracy"+str(accuracy))
-# loss0.059739165008068085
-# accuracy0.9853000044822693
+# loss0.04554622620344162
+# accuracy0.9865000247955322
 
 index = random.randint(0, len(X_test) - 1)
 sample_image = X_test[index]
